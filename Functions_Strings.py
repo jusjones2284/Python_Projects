8b63beabf05d595343b21b088fa4f2273aeeed3f
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-11-02 21:50:40 -0400

    working in python

---
+++ Functions_Strings.py
@@ -0,0 +1,23 @@
+s = "Welcome"
+len(s)
+print(len(s))
+
+s = input("Enter a string: ")
+
+if len(s) % 2 == 0:
+  print(s, "contains an even number of characters")
+else:
+  print(s, "contains an odd number of characters")
+
+
+s1 = "Welcome to python"
+s2 = "to"
+len(s1)
+
+print(max("Programming is fun"))
+print(s1[0])
+print(s1[-1])
+sliced = s1[0:3]
+print(sliced )
+
+s1[1:-1]
