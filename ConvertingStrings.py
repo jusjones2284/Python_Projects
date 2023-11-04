c0d7b9966a9a220e5f320dbd74f16a84c28516cc
Justin Jones <jus.jones@yahoo.com>
2023-11-04 17:55:57 -0400

    updated python

---
+++ ConvertingStrings.py
@@ -0,0 +1,34 @@
+# jbp = "The Joe Budden podcast"
+# capitalize("new joe Budden")
+# len(jbp)
+# lower("hello world")
+
+s = " welcome to python\t"
+# s1 = s.capitalize()
+# s2 = s.title()
+# s3 = s.lower()
+# s4 = s.upper()
+# s5 = s.swapcase()
+# s7 = "ABABAB".replace("AB", "ET", 2)
+
+j = s.lstrip()
+jj = s.rstrip()
+jmj = s.strip()
+
+jus = input("Enter a string").rstrip()
+
+
+print(jus)
+
+s1 = " Welcome "
+s2 = " welcome "
+
+isEqual = s1 == s2
+isEqual = s1.lower() == s2.lower()
+b = s1.startswith("AAA")
+bs = s1.endswith("aaa")
+x = s1[0]
+s3 = s1 + s2
+s3 = s1.lower()
+s1[1:]
+x = s1.find('e')
