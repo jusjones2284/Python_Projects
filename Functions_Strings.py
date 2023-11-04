2124022a6d3ec41ceadf728d3c8fb85a6514d9e4
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-11-03 21:20:08 -0400

    updating python

--- Functions_Strings.py
+++ Functions_Strings.py
@@ -1,23 +1,40 @@
-s = "Welcome"
-len(s)
-print(len(s))
+# s = "Welcome"
+# len(s)
+# print(len(s))
 
-s = input("Enter a string: ")
+# s = input("Enter a string: ")
 
-if len(s) % 2 == 0:
-  print(s, "contains an even number of characters")
-else:
-  print(s, "contains an odd number of characters")
+# if len(s) % 2 == 0:
+#   print(s, "contains an even number of characters")
+# else:
+#   print(s, "contains an odd number of characters")
 
 
-s1 = "Welcome to python"
-s2 = "to"
-len(s1)
+# s1 = "Welcome to python"
+# s2 = "to"
+# len(s1)
 
-print(max("Programming is fun"))
-print(s1[0])
-print(s1[-1])
-sliced = s1[0:3]
-print(sliced )
+# print(max("Programming is fun"))
+# print(s1[0])
+# print(s1[-1])
+# sliced = s1[0:3]
+# print(sliced )
 
-s1[1:-1]
+# s1[1:-1]
+
+# response = (input("Do you want to continue(Enter yes or no)")).lower()
+# # response = response.lower()
+
+# if response == "yes":
+#   print("Continuing...")
+# elif response == "no":
+#   print("Exiting...")
+# else:
+#   print("invaild response")
+
+# S = input("Enter a String: ")
+# if S.isdigit():
+#   print(S, "is a numberic string")
+# else:
+#     print("isnt a numberic string")
+
