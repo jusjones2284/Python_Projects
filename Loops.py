b0c0c13660066b746a9f8239f2d2b9cc518732d2
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-11-11 00:45:57 -0500

    update

--- Loops.py
+++ Loops.py
@@ -29,18 +29,46 @@ import random
 #   i = i + 1
 # print("sum is", sum)
 
-number1 = random.randint(0, 9)
-number2 = random.randint(0, 9)
+# number1 = random.randint(0, 9)
+# number2 = random.randint(0, 9)
 
-if number1 < number2:
-  number1, number2 = number2, number1
+# if number1 < number2:
+#   number1, number2 = number2, number1
 
-cA = number1 - number2
+# cA = number1 - number2
 
-answer = int(input("what is " + str(number1) + 
-                   " - " + str(number2) + "? "))
-if(cA == answer):
-  print("You got it!")
-else:
-  print("Game over")
+# answer = int(input("what is " + str(number1) + 
+#                    " - " + str(number2) + "? "))
+# if(cA == answer):
+#   print("You got it!")
+# else:
+#   print("Game over")
 
+# count = 1 
+# while count < 4: 
+#   count += 1
+#   print(count, end= '$ ')
+
+# number = random.randint(0, 100)
+# print("Guess a magic number between 0 and 100")
+
+# guess = int(input("Enter your guess: "))
+
+# if guess == number: 
+#   print("yes, the number is " + str(number))
+# elif guess < number:
+#   print("Your guess is too high")
+# else: 
+#   print("Your guess is too low")
+
+number = random.randint(0, 10)
+
+while True:
+    guesses = int(input("Enter your guess: "))
+
+    if guesses == number:
+      print("Yes, the number is", number)
+    elif guesses > number: 
+      print("number is too high")
+    else:
+      print("Your guess is too low")
