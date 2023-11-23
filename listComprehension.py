c3a5416424c5f47862a5a6399ee1a117657959f1
Justin Jones <jus.jones@yahoo.com>
2023-11-23 13:20:58 -0500

    updating python

---
+++ listComprehension.py
@@ -0,0 +1,53 @@
+# squares = []
+# for x in range(1, 101):
+#   squares.append(x**2)
+# # print(squares)
+
+# squares2 = [i**2 for i in range(1, 101)]
+# # print(squares2)
+
+# squares3 = [5 for x in range(3, 5)]
+# print(squares3)
+
+# movies = ["Star Wars", "Ghadhi", "Casablanca", "shawshank", "Gattaca", "To Kill a Mocking bird"]
+
+# gmovies = []
+
+# for title in movies: 
+#   if title.startswith("G"):
+#     gmovies.append(title)
+
+# print(gmovies)
+
+# gmovies1 = [title for title in movies if title.startswith("G")]
+
+# v = [2, -3, 1]
+
+# w = [4*x for x in v]
+
+# list20 = [30,1,2,1,0]
+
+# newList  = [x for x in list20 if x > 1]
+# print(newList)
+
+# [x for x in range(0, 10, 2)]
+# [x for x in range(10, 0, -2)]
+
+# print(list20.count(30))
+
+# newnewList =list20.extend(movies)
+# print(newnewList)
+
+# list1 = [2, 25, 3, 4, 1, 32, 4, 19, 99, 54]
+# list1.pop(4)
+# list1.remove(32)
+# list1.reverse()
+
+items = "Areebah Ashley Gabriel Helena".split()
+
+print(items)
+
+items1 = "12/25/1997".split("/")
+
+
+
